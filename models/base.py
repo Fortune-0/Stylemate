6e770d72a03f4edba848f0096ec5954dd0c60dbd
fortune-0 <fortunepeterspc07@gmail.com>
2024-04-23 16:37:05 +0100

    added the save and delete instance

--- models/base.py
+++ models/base.py
@@ -2,6 +2,8 @@
 """Create Base and parent class for Top and Bottom"""
 from sqlalchemy import Column, Boolean, ForeignKey, Integer, String
 from sqlalchemy.orm import declarative_base
+import models
+import uuid
 
 Base = declarative_base()
 
@@ -11,3 +13,13 @@ class BaseItem(Base):
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     category = Column(ForeignKey("category.name"), nullable=False)
     number = Column(Integer, nullable=False)
+    
+    def save(self):
+        """Update the attribute with the current date and time"""
+        self.updated_at = datetime.utcnow()
+        models.storage.new(self)
+        models.storage.save()
+        
+    def delete(self):
+        """Delete the instance from storage"""
+        models.storage.delete(self)
