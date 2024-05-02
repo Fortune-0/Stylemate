2d48707ed5b048756c45af3a397a499f8852a04d
fortune-0 <fortunepeterspc07@gmail.com>
2024-05-02 07:11:52 +0100

    update

--- console.py
+++ console.py
@@ -17,13 +17,25 @@ class StyleMate(cmd.Cmd):
     intro = "Welcome to the StyleMate Command Line Interface!\n\nType help or ? to list all available commands.\n"\
             "To exit type exit or press Ctrl+D.\n\n"
     classes = {"base": BaseItem, 'top': Top, 'bottoms': Bottom, 'database': Database}
-
-    def __init__(self):
-        super().__init__()
-        username = "user",
-        password = ""
-        self.database = Database(self, username, password)
-            
+       
+    # username = "",
+    # password = ""
+    # database = Database(username, password)
+    
+    def do_category(self, database):
+        """get category"""
+        try:
+            categories = database.get_cty()
+            if categories:
+                print("Available categories:")
+            for category in categories:
+                print(f"- {category}")
+                
+            else:
+                print(" NO CATEGORIES FOUND IN DATABASE")
+                
+        except Exception as e:
+            print(f"Error retriving database {str(e)}")
                   
     def do_exit(self, *args):
         """Exit the program and return to shell"""
@@ -65,26 +77,39 @@ class StyleMate(cmd.Cmd):
              
         pass
     
-    def do_delete(self, item_id):
+    def do_delete(self, database, table_name, item_id):
         """ Delete item with the item_id from database"""
         try:
-            conn = mysql.connector.connect(
-                host = "localhost",
-                user = "root",
-                database = "StyleMate_db",
-                password = "password"
-            )
-            cursor = conn.cursor()
-            
-            cursor.execute(f"Delete selected item with the {item_id} from the database")
-            
-            conn.commit()
-            
-            print(f"Item with {item_id} has been deleted.")
+            item = database.get_item_by_id(table_name, item_id)
+            if item:
+                database.delete_item(table_name, item_id)
+                print(f"{item_id} deleted from {table_name}")
+                
+            else:
+                print(f" No item with ID {item_id} in table {table_name}")
+       
         except Exception as e:
-            print(f"Error deleting item: {str(e)}")
-        finally:
-            conn.close()
+            print(f"Unexpected error: {e}")
+            
+            
+    #     try:
+    #         conn = mysql.connector.connect(
+    #             host = "localhost",
+    #             user = "root",
+    #             database = "StyleMate_db",
+    #             password = "password"
+    #         )
+    #         cursor = conn.cursor()
+            
+    #         cursor.execute(f"Delete selected item with the {item_id} from the database")
+            
+    #         conn.commit()
+            
+    #         print(f"Item with {item_id} has been deleted.")
+    #     except Exception as e:
+    #         print(f"Error deleting item: {str(e)}")
+    #     finally:
+    #         conn.close()
     
     def do_total(self, count_id):
         """Prints the total number of outfits avaliable"""
@@ -110,17 +135,28 @@ class StyleMate(cmd.Cmd):
             
         finally:
             conn.close()
-    
-    def do_get_user_sex(self, *args):
+    def do_add():
+        pass
+    def do_gender(self, database, user_id):
         """ Gets the gender of the User """
         try:
-            user_sex = self.__Session__.query(Database.table_classes.get("User")).first()
-            if user_sex:
-                print(f"Gender: {user_sex}")
+            
+            gender = database.get_gender(user_id)
+            if gender:
+                print(f"User is a: {gender}")
             else:
-                print("Current User not found in Database, please sign-up")
+                print(f"Gender infomation for {user_id} not found in the database.")
         except Exception as e:
-                print(f"Error retriving User's gender: {str(e)} ")
+            print(f" An error Occured {str(e)} .")
+            
+        # try:
+        #     user_sex = self.__Session__.query(Database.table_classes.get("User")).first()
+        #     if user_sex:
+        #         print(f"Gender: {user_sex}")
+        #     else:
+        #         print("Current User not found in Database, please sign-up")
+        # except Exception as e:
+        #         print(f"Error retriving User's gender: {str(e)} ")
         
     def  do_get_user_name(self, *args):
         """ Gets the user name of the current User """
