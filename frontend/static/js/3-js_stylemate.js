bd93a9376a6b0578b1b56d96e609bc9ba08712ef
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-05-04 23:49:59 +0100

    Make changes with frontend

--- frontend/static/js/3-js_stylemate.js
+++ frontend/static/js/3-js_stylemate.js
@@ -1,40 +1,21 @@
 $(document).ready(function () {
-  $('button.submit').click(function () {
-    $('#describeform1').trigger('submit');
-  });
-  $('#describeform1').submit(function (event) {
-    event.preventDefault();
-    const descForm1 = new FormData(this);
-    $.ajax({
-      url: 'http://0.0.0.0:5000/api/v1/get_description',
-      method: 'POST',
-      data: descForm1,
-      processData: false,
-      contentType: false,
-      success: function (response) {
-        $('#submit_status').text(JSON.stringify(response));
-        $('#submit_status').show(10);
-      },
-      error: function (err, errStr) {
-        $('button.sub').text(errStr);
-      }
+    $('#describeform1, #describeform2').submit(function (event) {
+        event.preventDefault();
+        const descForm = new FormData(this);
+        $.ajax({
+            url: 'http://0.0.0.0:5000/api/v1/get_description',
+            method: 'POST',
+            data: descForm,
+            processData: false,
+            contentType: false,
+            success: function (response) {
+                $('#submit_status').text(JSON.stringify(response));
+                $('#submit_status').show(10);
+            },
+            error: function (err, errStr) {
+                $('#submit_status').text(errStr);
+                $('#submit_status').show(10);
+            }
+        });
     });
-  });
-  $('#describeform2').submit(function (event) {
-    event.preventDefault();
-    const descForm2 = new FormData(this);
-    $.ajax({
-      url: 'http://0.0.0.0:5000/api/v1/get_description',
-      method: 'POST',
-      data: descForm2,
-      processData: false,
-      contentType: false,
-      success: function (response) {
-        $('#submit_status').show(10);
-      },
-      error: function (err, errStr) {
-        $('button.sub').text(errStr);
-      }
-    });
-  });
 });
