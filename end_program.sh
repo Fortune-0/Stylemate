1c361a392dc817bfaeb17be958b85fe895b4b9a3
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-05-10 11:05:24 +0100

    Implementend-program mechanism

---
+++ end_program.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# Set up the environment for Stylemate
+
+tmux kill-session -t stylemate_web_app
+tmux kill-session -t stylemate_api
--- set_up.sh
+++ set_up.sh
@@ -3,5 +3,5 @@
 
 sudo service mysql start
 sudo cat setup_mysql.sql | mysql -uroot -p
-tmux new-session -c ./frontend -d 'python3 -m web_app'
-tmux new-session -d 'python3 -m api.v1.stylemate_app'
+tmux new-session -s stylemate_web_app -c ./frontend -d 'python3 -m web_app'
+tmux new-session -s stylemate_api -d 'python3 -m api.v1.stylemate_app'
